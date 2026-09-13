bae629fc12d43238b6c90fe923e6b535837a2d59
abarhub <tulipebleu1@laposte.net>
2026-09-13 11:28:18 +0200

    recher dans le fichier

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,37 @@
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 
-fn main() -> io::Result<()> {
-    let file = File::open("LICENSE")?;
+use serde::Deserialize;
+use std::error::Error;
+
+#[derive(Debug, Deserialize)]
+struct Config {
+    fichier: String,
+    text: String,
+    debug: bool,
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let content = std::fs::read_to_string("config.toml")?;
+    let config: Config = toml::from_str(&content)?;
+
+    let file = File::open(config.fichier)?;
     let reader = BufReader::new(file);
+
+    let mut trouve = false;
 
     for line in reader.lines() {
         let line = line?;
-        println!("{}", line);
+        if (config.debug) {
+            println!("{}", line);
+        }
+        if line.contains(&config.text) {
+            trouve = true;
+            break;
+        }
     }
+
+    println!("trouve: {}", trouve);
 
     Ok(())
 }
